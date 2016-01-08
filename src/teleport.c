3fc858f82ecc93797b7987effc9093fe0c2ea850
Gan Quan <coin2028@hotmail.com>
2016-01-08 13:29:55 +0800

    sam will now teleport to you if you teleport him

--- src/teleport.c
+++ src/teleport.c
@@ -1723,7 +1723,11 @@ boolean give_feedback;
 		You("are no longer inside %s!", mon_nam(mtmp));
 	    unstuck(mtmp);
 	    rloc(mtmp, FALSE);
-	} else if (is_rider(mtmp->data) && rn2(13) &&
+	} else if ((
+#ifdef BLACKMARKET
+		   mtmp->data == &mons[PM_BLACK_MARKETEER] ||
+#endif
+		   is_rider(mtmp->data)) && rn2(13) &&
 		   enexto(&cc, u.ux, u.uy, mtmp->data))
 	    rloc_to(mtmp, cc.x, cc.y);
 	else {
