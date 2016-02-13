a08185565d2c858ce0e649df3da363bda4e8f4ab
Gan Quan <coin2028@hotmail.com>
2016-02-13 16:47:04 +0800

    flying monsters can no longer pickup items in pits without diving

--- src/engrave.c
+++ src/engrave.c
@@ -117,7 +117,7 @@ boolean check_pit;
     /* Restricted/unskilled riders can't reach the floor */
     if (u.usteed && P_SKILL(P_RIDING) < P_BASIC)
         return FALSE;
-    if (check_pit && !Flying
+    if (check_pit
         && (t = t_at(u.ux, u.uy)) != 0 && uteetering_at_seen_pit(t))
         return FALSE;
 
